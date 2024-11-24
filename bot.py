351034cf4a6e91581b93240af490705fa071aaf0
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-11-25 00:19:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,39 +1,28 @@
-from flask import Flask
 from pyrogram import Client
+from config import *
 import os
 
-app = Flask(__name__)
+class Bot(Client):
 
-# Pyrogram client
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_NAME = os.getenv("BOT_NAME")
+    def __init__(self):
+        super().__init__(
+            name="simple",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=100,
+            plugins={"root": "plugins"},
+            sleep_threshold=10,
+        )
+    async def start(self):
+        await super().start()
+        me = await self.get_me()      
+        print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
 
-client = Client(
-    name=BOT_NAME,
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="plugins")
-)
+    async def stop(self, *args):
+       await super().stop()      
+       print("Bot Restarting........")
 
-# Flask route to start the bot
-@app.route("/start", methods=["GET"])
-def start_bot():
-    client.start()
-    return "Bot started successfully!"
 
-# Flask route to stop the bot
-@app.route("/stop", methods=["GET"])
-def stop_bot():
-    client.stop()
-    return "Bot stopped successfully!"
-
-# Flask route to handle requests to the root URL
-@app.route("/", methods=["GET"])
-def index():
-    return "Your bot is running!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
+bot = Bot()
+bot.run()
