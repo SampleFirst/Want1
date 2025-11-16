e2fe463c5b71e037561c9dd246ce880446f77e6b
Sample First <132287607+SampleFirst@users.noreply.github.com>
2025-11-16 16:49:29 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+from os import environ
+
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+ADMIN = int(environ.get("ADMIN", "5433924139"))          
+CAPTION = environ.get("CAPTION", "")
+
+# for thumbnail ( back end is MrMKN brain 😉)
+DOWNLOAD_LOCATION = "./DOWNLOADS"
+
